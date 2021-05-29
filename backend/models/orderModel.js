5c38bac2f884c4d6ee1484af0671cc90dbb74529
gride29 <cezario2000@gmail.com>
2021-05-29 21:04:48 +0200

    Order handling in backend

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -10,7 +10,7 @@ const orderSchema = mongoose.Schema(
 		orderItems: [
 			{
 				name: { type: String, required: true },
-				quantity: { type: Number, required: true },
+				qty: { type: Number, required: true },
 				image: { type: String, required: true },
 				price: { type: Number, required: true },
 				product: {
@@ -20,7 +20,7 @@ const orderSchema = mongoose.Schema(
 				},
 			},
 		],
-		shippingAdress: {
+		shippingAddress: {
 			address: { type: String, required: true },
 			city: { type: String, required: true },
 			postalCode: { type: String, required: true },
